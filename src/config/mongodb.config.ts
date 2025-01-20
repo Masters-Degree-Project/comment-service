7adaa190b8f53edd1c7f3db6814263b2e4ec202a
melih <dev.mberatsanli@gmail.com>
2025-01-20 23:14:08 +0100

    docker mongo adjusted

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -1,5 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('mongodb', () => ({
-  uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/comment-service',
-})); 
+  uri:
+    `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` ||
+    'mongodb://localhost:27017/comment-service',
+}));
