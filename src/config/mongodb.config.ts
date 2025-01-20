22e8b93d609f5e958ab56a67da1de903803acd67
melih <dev.mberatsanli@gmail.com>
2025-01-20 22:19:21 +0100

    docker container key

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -2,4 +2,4 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('mongodb', () => ({
   uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/comment-service',
-})); 
+}));
